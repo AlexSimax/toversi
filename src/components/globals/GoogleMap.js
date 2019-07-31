dcbaca1a3bfd1d689a6bff9cacf0e1b20e58875b
Alex Simax <eurock3@gmail.com>
2019-07-31 13:32:03 +0300

    enviroment test

--- src/components/globals/GoogleMap.js
+++ src/components/globals/GoogleMap.js
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <div id="map">
       <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.GOOGLE_API_KEY}`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${GOOGLE_API_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100vh` }} />}
         mapElement={<div style={{ height: `100%` }} />}
