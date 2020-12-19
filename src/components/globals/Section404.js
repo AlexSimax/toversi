77f1b15cdcc3afc63ce035086ea85cc8a5d119e0
Alex Simakov <eurock@MacBook-Pro.local>
2020-12-19 09:06:55 +0300

    with 404

--- src/components/globals/Section404.js
+++ src/components/globals/Section404.js
@@ -2,13 +2,29 @@ import React from "react"
 import styled from "styled-components"
 import SEO from "../../components/seo"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function Section404() {
+  const data = useStaticQuery(graphql`
+    query {
+      img1: file(relativePath: { eq: "404.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
   return (
     <Section404Wrapper>
-      <SEO title="404: Not found" />
-      <h1>Ничего не найдено. Увы...</h1>
-      <p>Набранный Вами путь не существует... сожалеем.</p>
+      <div class="wrapper_img">
+        <SEO title="404: Not found" />
+        {/* <h1>Ничего не найдено. Увы...</h1>
+        <p>Набранный Вами путь не существует... сожалеем.</p> */}
+        <Img fluid={data.img1.childImageSharp.fluid} alt="not-found" />
+      </div>
       <Link to="/">
         <button>На Главную</button>
       </Link>
@@ -42,11 +58,19 @@ const Section404Wrapper = styled.div`
     border: 2px solid grey;
     border-radius: 7px;
     outline: none;
-    margin: 0 0 0 60%;
+    margin: 3% 0 2% 40%;
   }
   button:hover {
-    color: #1ec515;
-    padding: 15px 38px;
+    color: #d4af37;
     cursor: pointer;
+  }
+  @media (max-width: 576px) {
+    button {
+      font-size: 14px;
+      padding: 7px 15px;
+      margin-top: 7px;
+      margin-bottom: 7px;
+      margin: 3% 0 2% 30%;
+    }
   }
 `
--- src/pages/404.js
+++ src/pages/404.js
@@ -4,9 +4,9 @@ import Layout from "../components/layout"
 import Section404 from "../components/globals/Section404"
 
 const NotFoundPage = () => (
-  <Layout>
-    <Section404 />
-  </Layout>
+  // <Layout>
+  <Section404 />
+  // </Layout>
 )
 
 export default NotFoundPage
